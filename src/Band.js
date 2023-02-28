2028d3d41152bbd380975c5250a75ab6e5b4a24f
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-28 14:35:37 -0500

    Challenge 10

--- src/Band.js
+++ src/Band.js
@@ -4,15 +4,19 @@ import "./Band.css";
 
 const Band = ({ band }) => {
   // Use object destructuring to get the properties from the band object
-  const { band_name, formed, origin, fans } = band;
+  const { band_name, formed, origin, fans, split, style } = band;
+
+  const likesIfNotSplit = split === "-" ? <Like /> : <p></p>;
 
   return (
     <div className='band'>
       <h3>{band_name}</h3>
-      <p>Formed: {formed}</p>
-      <p>{origin}</p>
+      <p>Origin: {origin}</p>
       <p>Fans: {fans}</p>
-      <Like />
+      <p>Formed: {formed}</p>
+      <p>Split: {split}</p>
+      <p>Style: {style}</p>
+      {likesIfNotSplit}
     </div>
   );
 };
