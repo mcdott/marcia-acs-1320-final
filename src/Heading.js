9e078fcf0e225495d34012eae88165033ec5b7fd
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-28 12:58:18 -0500

    Challenges 1 and 2

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className='Heading'>
+      <header>
+        <h1>Marcia</h1>
+        <h2>ACS 1320 Final Assessment</h2>
+      </header>
+    </div>
+  );
+}
+
+export default Heading;
