0c135e418018454d387233ed5bf9379f70f50653
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-28 14:41:57 -0500

    Stretch Challenge 2

--- src/Band.js
+++ src/Band.js
@@ -8,11 +8,13 @@ const Band = ({ band }) => {
 
   const likesIfNotSplit = split === "-" ? <Like /> : <p></p>;
 
+  let fans_full_num = fans * 1000;
+
   return (
     <div className='band'>
       <h3>{band_name}</h3>
       <p>Origin: {origin}</p>
-      <p>Fans: {fans}</p>
+      <p>Fans: {fans_full_num.toLocaleString()}</p>
       <p>Formed: {formed}</p>
       <p>Split: {split}</p>
       <p>Style: {style}</p>
